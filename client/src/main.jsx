b1dc6a9df4f533dd8593b488e5fef6dd4932a139
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-03-30 16:09:34 +0530

    routers added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import MainLayout from "./layouts/MainLayout.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/home",
+        element: <div><Home/></div>,
+      },
+      {
+        path: "/about",
+        element: <div><About/></div>,
+      }
+    ],
+    errorElement: <div>Error</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
